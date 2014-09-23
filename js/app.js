92d104c379db29fe8bb003fc1c6bb007c499b27f
Jesse Showalter <iamjesseshowalter@gmail.com>
2014-09-22 15:25:17 -1000

    created first 2 functions
    createSecretNum() and newGame()

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -12,22 +11,44 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(500);
   	});
 
+  	$(".new").click(function(){
+  		newGame();
+  	});
+
+  	newGame();
 });
 
+var createSecretNum = function(secretNum){
+	secretNum = Math.floor(Math.random() * 100 + 1);
+	console.log("Generated Random Number = " + secretNum);
+};
+
+
+var newGame = function(){
+	console.log("Created a new game");
+	createSecretNum();
+};
+
+
+
+
+
+
+
+// ****** Requirments Breakdown *******
 // create a newGame function to run on pageload and on button push
 // create a chooseNumber function to pick a random number between 1 and 100
 // write a named function that recieves user guess and determines which conditional feedback to provide
 // create absolute values(logic) in correspondance to the secret numbr and the guessed number (hot cold, hotter colder, really hot! really cold!)
 
-// feedback should replce text in the h2#feedback
+// feedback should replace text in the h2#feedback
 // amount of guesses apperas in span.#count
 // each previusly guessed number appended onto the ul#guestList
 // create check for numerical input for each guess, if wrong display error message
 
 
 
-// ****** functions needed *******
-
+// ****** Needed Functions *******
 // newGame()
 // createSecretNum()
 // checkGuess()
