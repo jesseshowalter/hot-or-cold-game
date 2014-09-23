900ee35bdc467b7b97046ea53c1143b13f9f3ce7
Jesse Showalter <iamjesseshowalter@gmail.com>
2014-09-22 16:11:51 -1000

    writing checkGuess()
    extending functions to store values
    
    fixed scope issue

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -11,33 +12,83 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(500);
   	});
 
+  	/*--- Create a new Game on button click ---*/
   	$(".new").click(function(){
   		newGame();
   	});
 
-  	newGame();
+  	/*--- Create the secret number ---*/
+  	var createSecretNum = function(){
+		secretNum = Math.floor(Math.random() * 100 + 1);
+		console.log("Generated Random Number = " + secretNum);
+	};
+
+	/*--- Create a new Game ---*/
+	var newGame = function(){
+		console.log("Created a new game");
+		createSecretNum();
+	};
+	newGame();
+
+	/*--- Check user inputed gues and run conditional logic ---*/
+	var checkGuess = function(){
+		$("#guessButton").click(function(e){
+			e.preventDefault(); // stops page reload
+			var userGuess = $("#userGuess").val(); // stored users input
+			
+			// if(){
+
+			// }else if{
+
+			// }else if{
+
+			// }else{
+
+			// };
+			console.log(secretNum);
+
+			$("#userGuess").val(""); // clears the form field for next guess
+		});
+	};
+	checkGuess();
+
 });
 
-var createSecretNum = function(secretNum){
-	secretNum = Math.floor(Math.random() * 100 + 1);
-	console.log("Generated Random Number = " + secretNum);
-};
-
-
-var newGame = function(){
-	console.log("Created a new game");
-	createSecretNum();
-};
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+// ****** Needed Functions *******
+//** Done **
+// newGame() ** Done **
+// createSecretNum() ** Done **
+
+
+// checkGuess()
+// hotOrCold()
+// guessFeedback()
+// countGuesses()
+// listGuesses()
+// validateGuess
 
 
 // ****** Requirments Breakdown *******
-// create a newGame function to run on pageload and on button push
-// create a chooseNumber function to pick a random number between 1 and 100
+// ** Done ** 
+// create a newGame function to run on pageload and on button push   ** Done ** 
+// create a chooseNumber function to pick a random number between 1 and 100  ** Done **
+
+
+// ** Still need to be done **
 // write a named function that recieves user guess and determines which conditional feedback to provide
 // create absolute values(logic) in correspondance to the secret numbr and the guessed number (hot cold, hotter colder, really hot! really cold!)
 
@@ -48,12 +99,4 @@ var newGame = function(){
 
 
 
-// ****** Needed Functions *******
-// newGame()
-// createSecretNum()
-// checkGuess()
-// hotOrCold()
-// guessFeedback()
-// countGuesses()
-// listGuesses()
-// validateGuess
+
